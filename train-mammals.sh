b00e6496ee8c6ee7adc5e143fcc9906c83820647
Me <sameer.bansal@ed.ac.uk>
2019-07-01 04:49:07 +0100

    code walkthrough

--- train-mammals.sh
+++ train-mammals.sh
@@ -6,17 +6,18 @@
 # LICENSE file in the root directory of this source tree.
 
 python3 embed.py \
-       -dim 5 \
+       -dim 10 \
        -lr 0.3 \
        -epochs 300 \
        -negs 50 \
        -burnin 20 \
-       -ndproc 4 \
+       -ndproc 1 \
        -manifold poincare \
        -dset wordnet/mammal_closure.csv \
-       -checkpoint mammals.pth \
+       -checkpoint mammals_poincare_dim10.pth \
        -batchsize 10 \
        -eval_each 1 \
        -fresh \
        -sparse \
-       -train_threads 2
+       -gpu -1 \
+       -train_threads 1
